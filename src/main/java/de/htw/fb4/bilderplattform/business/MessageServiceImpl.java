50473da706780aa0b75c38cd6bec94140456f0d7
s0538297 <s0538297@Julian-Wolferts-MacBook-Air.local>
2012-11-10 14:05:21 +0000

    Message added

---
+++ src/main/java/de/htw/fb4/bilderplattform/business/MessageServiceImpl.java
@@ -0,0 +1,71 @@
+package de.htw.fb4.bilderplattform.business;
+
+import java.util.Calendar;
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import de.htw.fb4.bilderplattform.dao.Message;
+import de.htw.fb4.bilderplattform.dao.MessageDAOImpl;
+import de.htw.fb4.bilderplattform.spring.context.ApplicationContextProvider;
+
+class MessageServiceImpl implements IMessageService {
+
+	
+	@Override
+	@Transactional
+	public void saveMessage(Message message){
+		if(message == null){
+			return;
+		}
+		MessageDAOImpl messageDAO = ApplicationContextProvider
+				.getApplicationContext().getBean("messageDAO", MessageDAOImpl.class);
+		messageDAO.saveMessage(message);
+	}
+	
+	
+	
+	/*
+	@Override
+	public User getUserByName(String username) throws UsernameNotFoundException {
+		UserDAOImpl userDAO = ApplicationContextProvider
+				.getApplicationContext().getBean("userDao", UserDAOImpl.class);
+		List<User> allUser = userDAO.getAllUser();
+		for (User usr : allUser) {
+			if (usr.getUsername().equals(username))
+				return usr;
+		}
+		throw new UsernameNotFoundException(username);
+	}
+
+	@Override
+	public List<User> getAllUser() throws UsernameNotFoundException {
+		UserDAOImpl userDAO = ApplicationContextProvider
+				.getApplicationContext().getBean("userDao", UserDAOImpl.class);
+		return userDAO.getAllUser();
+	}
+
+	@Override
+	@Transactional
+	public void saveOrUpdateUser(User user) {
+		if (user == null) {
+			return;
+		}
+		UserDAOImpl userDAO = ApplicationContextProvider
+				.getApplicationContext().getBean("userDao", UserDAOImpl.class);
+		user.setLastUpdateDate(Calendar.getInstance().getTime());
+		userDAO.saveUser(user);
+	}
+
+	@Override
+	public void deleteUser(User user) {
+		if (user == null) {
+			return;
+		}
+		UserDAOImpl userDAO = ApplicationContextProvider
+				.getApplicationContext().getBean("userDao", UserDAOImpl.class);
+		userDAO.deleteUser(user);
+	}
+	*/
+
+}
