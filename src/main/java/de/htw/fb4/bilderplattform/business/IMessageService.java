f45368b2bb123aed26ac369d2265404d7a1d2b13
s0538297 <s0538297@Julian-Wolferts-MacBook-Air.local>
2012-11-10 14:05:54 +0000

    Message added

---
+++ src/main/java/de/htw/fb4/bilderplattform/business/IMessageService.java
@@ -0,0 +1,25 @@
+package de.htw.fb4.bilderplattform.business;
+
+import java.util.List;
+
+import de.htw.fb4.bilderplattform.dao.Message;
+
+
+public interface IMessageService {
+
+	public void saveMessage(Message message);
+	
+	//public List<Message> getAllMessagesTopic
+	
+	
+	
+/*
+	public User getUserByName(String username) throws UsernameNotFoundException;
+
+	public List<User> getAllUser() throws UsernameNotFoundException;
+	
+	public void saveOrUpdateUser(User user);
+	
+	public void deleteUser(User user);
+*/	
+}
