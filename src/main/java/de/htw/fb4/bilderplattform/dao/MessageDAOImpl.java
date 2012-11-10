3afc8279cffe5954d2e1d82f8e85010ce18de52f
s0538297 <s0538297@Julian-Wolferts-MacBook-Air.local>
2012-11-10 10:58:23 +0000

    Message DAO added

---
+++ src/main/java/de/htw/fb4/bilderplattform/dao/MessageDAOImpl.java
@@ -0,0 +1,26 @@
+package de.htw.fb4.bilderplattform.dao;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+/************************************************
+ * <p>message DAO</p>
+ * 
+ * <p>
+ * @author ben
+ * </p>
+ * <p>
+ * 10.10.2012
+ * </p>
+ ************************************************/
+public class MessageDAOImpl extends AbstractDAO {
+
+
+	
+	
+	
+}
+
+
