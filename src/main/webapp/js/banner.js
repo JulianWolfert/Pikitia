004ef2aa723b4ce7e65f2b1005fb22fa986cd914
s0523841 <s0523841@Julian-Wolferts-MacBook-Air.local>
2012-12-14 22:52:12 +0000

    banner update

--- src/main/webapp/js/banner.js
+++ src/main/webapp/js/banner.js
@@ -3,39 +3,39 @@
 //configuration
 var intervall = 3000;
 
-//do not change values!
+// do not change values!
 var autoSwitcherOn = true;
-var animationIsRunning=false;
+var animationIsRunning = false;
 var autoSwitcher = window.setInterval("nextImg()", intervall);
 
 window.onload = init;
 
 function init() {
 	var $first = $('.banner_imglist img').first();
-	var banner_height=$('.banner_imglist').height();
-	var banner_width=$('.banner_imglist').width();
-	
-	$('.banner_imglist img').each( function(){
+	var banner_height = $('.banner_imglist').height();
+	var banner_width = $('.banner_imglist').width();
+
+	$('.banner_imglist img').each(function() {
 		img_height = $(this).height();
 		img_width = $(this).width();
 		img_quotient = img_width / img_height;
 		banner_quotient = banner_width / banner_height;
 
-		if(img_quotient < banner_quotient){
+		if (img_quotient < banner_quotient) {
 			scale = banner_width / img_width;
 			$(this).height(img_height * scale);
 			$(this).width(img_width * scale);
-			img_magin_top = "-"+($(this).height-banner_height)/2+"px";
-			$(this).css("margin-top",img_magin_top);
-		}else{
+			img_magin_top = "-" + ($(this).height - banner_height) / 2 + "px";
+			$(this).css("margin-top", img_magin_top);
+		} else {
 			scale = banner_height / img_height;
 			$(this).height(img_height * scale);
 			$(this).width(img_width * scale);
-			img_magin_left = "-"+($(this).width-banner_width)/2+"px";
-			$(this).css("margin-left",img_magin_left);
+			img_magin_left = "-" + ($(this).width - banner_width) / 2 + "px";
+			$(this).css("margin-left", img_magin_left);
 		}
 	});
-	
+
 	$first.addClass('active');
 	$first.show();
 }
@@ -44,20 +44,22 @@ function resetAutoSwitcher() {
 	window.clearInterval(autoSwitcher);
 	autoSwitcher = window.setInterval("nextImg()", intervall);
 	autoSwitcherOn = true;
+	$(".banner_play_pause").attr("src", "images/banner/pause.png");
 }
 
 function stopAutoSwitcher() {
 	if (autoSwitcherOn) {
 		window.clearInterval(autoSwitcher);
 		autoSwitcherOn = false;
+		$(".banner_play_pause").attr("src", "images/banner/play.png");
 	}
 }
 
 function nextImg() {
-	if(animationIsRunning){
+	if (animationIsRunning) {
 		return;
 	}
-	
+
 	var $active = $('.banner_imglist img.active');
 	var $next = $active.next();
 
@@ -67,13 +69,19 @@ function nextImg() {
 	$next.addClass('active');
 	$active.removeClass('active');
 
-	animationIsRunning=true;
-	$next.stop().show("slide", { direction: "left" }, 1000);
-	$active.stop().hide("slide", { direction: "right" }, 1000, function(){animationIsRunning=false;});
+	animationIsRunning = true;
+	$next.stop().show("slide", {
+		direction : "left"
+	}, 1000);
+	$active.stop().hide("slide", {
+		direction : "right"
+	}, 1000, function() {
+		animationIsRunning = false;
+	});
 }
 
 function prevImg() {
-	if(animationIsRunning){
+	if (animationIsRunning) {
 		return;
 	}
 
@@ -86,9 +94,15 @@ function prevImg() {
 	$prev.addClass('active');
 	$active.removeClass('active');
 
-	animationIsRunning=true;
-	$prev.show("slide", { direction: "right" }, 1000);
-	$active.stop().hide("slide", { direction: "left" }, 1000, function(){animationIsRunning=false;});
+	animationIsRunning = true;
+	$prev.show("slide", {
+		direction : "right"
+	}, 1000);
+	$active.stop().hide("slide", {
+		direction : "left"
+	}, 1000, function() {
+		animationIsRunning = false;
+	});
 }
 
 function clickNext() {
@@ -101,7 +115,11 @@ function clickPrev() {
 	prevImg();
 }
 
-function clickPlay() {
-	nextImg();
-	resetAutoSwitcher();
+function clickPlayPause() {
+	if (!autoSwitcherOn) {
+		nextImg();
+		resetAutoSwitcher();
+	} else {
+		stopAutoSwitcher();
+	}
 }
