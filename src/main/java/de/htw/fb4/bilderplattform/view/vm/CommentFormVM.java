78caa30f635f08845ac0035c3695cc6b06a6ceec
s0523891 <s0523891@Julian-Wolferts-MacBook-Air.local>
2013-01-07 16:29:26 +0000

    Bewertungsformular (CommentForm) Star Rating hinzugefügt.

--- src/main/java/de/htw/fb4/bilderplattform/view/vm/CommentFormVM.java
+++ src/main/java/de/htw/fb4/bilderplattform/view/vm/CommentFormVM.java
@@ -1,17 +1,17 @@
 package de.htw.fb4.bilderplattform.view.vm;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.zkoss.bind.BindUtils;
 import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.bind.annotation.ExecutionArgParam;
+import org.zkoss.zhtml.Li;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zk.ui.select.annotation.Wire;
@@ -42,20 +42,15 @@ public class CommentFormVM {
 	@Wire("#commentForm")
 	private Window win;
 
-	private ICommentService commentService = BusinessCtx.getInstance().getCommentService();
-	private IUserService userService = BusinessCtx.getInstance().getUserService();
+	private ICommentService commentService = BusinessCtx.getInstance()
+			.getCommentService();
+	private IUserService userService = BusinessCtx.getInstance()
+			.getUserService();
 
-	private static List<Integer> stars = new ArrayList<Integer>();
+	@Wire
+	private Li starCssStyle;
 	
-	static{
-		stars.add(1);
-		stars.add(2);
-		stars.add(3);
-		stars.add(4);
-		stars.add(5);
-    }
-	
-	private Integer selectedStarValue;
+	private Integer currentStarValue;
 	private String text;
 	private Integer idImage;
 	private String username;
@@ -63,46 +58,45 @@ public class CommentFormVM {
 	public CommentFormVM() {
 
 	}
-	
+
 	@AfterCompose
-	public void afterCompose(@ContextParam(ContextType.VIEW) Component view,  @ExecutionArgParam("imageID") String imageID) {
+	public void afterCompose(@ContextParam(ContextType.VIEW) Component view,
+			@ExecutionArgParam("imageID") String imageID) {
 		Selectors.wireComponents(view, this, false);
 		logger.debug("imageID: " + imageID + " was sent to commentForm.zul");
 		this.idImage = Integer.parseInt(imageID);
-		this.selectedStarValue = stars.get(0);
+		this.currentStarValue = 1;
 	}
 
-	public boolean isAUserAuthenticated(){
+	public boolean isAUserAuthenticated() {
 		return userService.isAUserAuthenticated();
 	}
-	
+
 	public String getText() {
 		return text;
 	}
 
 	public String getUsername() {
-		if(userService.isAUserAuthenticated()){
+		if (userService.isAUserAuthenticated()) {
 			return userService.getCurrentlyLoggedInUser().getUsername();
 		}
 		return username;
 	}
 
-	public List<Integer> getStars() {
-		return new ArrayList<Integer>(stars);
+	public Integer getCurrentStarValue() {
+//		System.out.println("getStarValue" + currentStarValue);
+		return currentStarValue;
 	}
 
-	public Integer getSelectedStarValue() {
-		return selectedStarValue;
-	}
-
-	public void setSelectedStarValue(Integer selectedStarValue) {
-		this.selectedStarValue = selectedStarValue;
+	public void setCurrentStarValue(Integer currentStarValue) {
+//		System.out.println("setCurrentStarValue" + currentStarValue);
+		this.currentStarValue = currentStarValue;
 	}
 
 	public void setText(String text) {
 		this.text = text;
 	}
-	
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
@@ -110,10 +104,11 @@ public class CommentFormVM {
 	@Command
 	public void submit() {
 		String usrname = this.username;
-		if(!userService.isAUserAuthenticated()){
-			usrname = this.username + " " + SpringPropertiesUtil.getProperty("lbl.anonymousUser");
+		if (!userService.isAUserAuthenticated()) {
+			usrname = this.username + " "
+					+ SpringPropertiesUtil.getProperty("lbl.anonymousUser");
 		}
-		Comment comment = new Comment(this.getSelectedStarValue(), this.getText(), this.idImage, usrname);
+		Comment comment = new Comment(this.getCurrentStarValue(), this.getText(), this.idImage, usrname);
 		commentService.saveOrUpdateComment(comment);
 		closeThis();
 	}
@@ -123,6 +118,13 @@ public class CommentFormVM {
 		Map args = new HashMap();
 		BindUtils.postGlobalCommand(null, null, "refresh", args);
 		win.detach();
+	}
+
+	@Command
+	public void setStarValue(@BindingParam("currentStarValue") final Integer currentStarValue, @BindingParam("cssStyle") final String cssStyle) {
+		this.starCssStyle.setStyle(cssStyle);
+		this.setCurrentStarValue(currentStarValue);
+//		System.out.println(" currentStarValue: " + currentStarValue);
 	}
 
 }
