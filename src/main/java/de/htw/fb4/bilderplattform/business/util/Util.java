9fb6e8711e0f3d788ff7962440ec8442ddb49a29
s0537867 <s0537867@Julian-Wolferts-MacBook-Air.local>
2013-01-08 17:49:16 +0000

    Verbesserung

--- src/main/java/de/htw/fb4/bilderplattform/business/util/Util.java
+++ src/main/java/de/htw/fb4/bilderplattform/business/util/Util.java
@@ -5,7 +5,7 @@ import java.text.DecimalFormat;
 public class Util {
 
 	public static final String formatDouble(double d){
-		DecimalFormat df = new DecimalFormat(".##0.00" );
+		DecimalFormat df = new DecimalFormat(",##0.00");
 		return df.format(d);
 	}
 	
