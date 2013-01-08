05232f77e6745a5ec343525219ecab62c6d5d9c4
s0537867 <s0537867@Julian-Wolferts-MacBook-Air.local>
2013-01-08 22:25:51 +0000

    Util

--- src/main/java/de/htw/fb4/bilderplattform/business/util/Util.java
+++ src/main/java/de/htw/fb4/bilderplattform/business/util/Util.java
@@ -4,8 +4,8 @@ import java.text.DecimalFormat;
 
 public class Util {
 
-	public static final String formatDouble(double d){
-		DecimalFormat df = new DecimalFormat(",##0.00");
+	public static String formatDouble(double d){
+		DecimalFormat df = new DecimalFormat("##0.00");
 		return df.format(d);
 	}
 	
