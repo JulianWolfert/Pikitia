b7ae1d9e22f9b3649ab5fbc6ce4f30b1dc2d0390
s0537867 <s0537867@Julian-Wolferts-MacBook-Air.local>
2013-01-08 17:48:13 +0000

    Util Klasse mit Formatierung für Dezimalzahl

---
+++ src/main/java/de/htw/fb4/bilderplattform/business/util/Util.java
@@ -0,0 +1,12 @@
+package de.htw.fb4.bilderplattform.business.util;
+
+import java.text.DecimalFormat;
+
+public class Util {
+
+	public static final String formatDouble(double d){
+		DecimalFormat df = new DecimalFormat(".##0.00" );
+		return df.format(d);
+	}
+	
+}
